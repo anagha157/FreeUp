3032e5ca4767b47d8207b3aa3e39d001b33bc76e
anagha157 <69710957+anagha157@users.noreply.github.com>
2022-04-01 14:41:30 +0530

    commit_1

--- db/config.php
+++ db/config.php
@@ -1,8 +1,14 @@
 <?php
+     $host = "localhost";
+    $user = "root";
+    $pw = "";
+    $db = "freeup";
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    $conn = mysqli_connect($host, $user, $pw, $db);
+    if(!$conn )
+    {
+        die("<script> alert (Connection Not Successful!); </script>");
+    }
+?>
+
 
