b1d8584b483b5e65e74220870658ccdf333a1f69
anagha157 <69710957+anagha157@users.noreply.github.com>
2022-04-02 00:03:43 +0530

    commit_5

--- login_submit.php
+++ login_submit.php
@@ -1,39 +1,32 @@
 <?php
     
-    require './db/config.php';
-
-    
+    include('db/config.php');
 
         $email = $_POST['email'];
         $password = md5($_POST['pswd']);
+        
+        
 
         $email_regex = "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/";
         
         if(!preg_match($email_regex , $email)){
-        $error = "<div class='alert text-danger bg-danger'>Incorrect email</div>";
-        header("location:login_submit.php?m2=".$error);
+        $error = "<div class='alert text-danger'>Incorrect email</div>";
+        header("location:demo_login.php?m2=".$error);
         }
-    
-       
-    
-        $q1 = "SELECT * from users where email = '$email' and password = '$password'";
-        $result = $conn -> query($q1);
-        $row = $result -> fetch_assoc();
-        $name = $row['name'];
-        $num_of_rows = mysqli_num_rows($result);
+             
+        $select_query = "SELECT * FROM users WHERE email ='$email' AND password = '$password' ";
         
+        $result = mysqli_query($conn , $select_query) or die(mysqli_error($conn));
+        $rows_fetched = mysqli_num_rows($result);
         
-        if($num_of_rows == 1)
+        if($rows_fetched!=0)
         {
-            $_SESSION['email'] = $email;
-            $_SESSION['name'] = $name;
+        
             header("location:index.php");
-
-            
         }
         else{
-            $error = "<div class='alert text-danger bg-danger'></div>";
-            header("location:login_submit.php?m2=".$error);
+            $error = "<div class='alert text-danger'>Invalid Credentials</div>";
+            header("location:demo_login.html?m1=".$error);
         }
     
 
